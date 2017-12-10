d1c7cca19955373e48266a4ff8f9c33d15a8ff31
Del-S <david@sucharda.cz>
2017-12-10 01:08:06 +0100

    Fingeprint markers and options added (new and existing)

--- mobile/src/main/java/cz/uhk/fim/kikm/wearnavigation/activities/scan/ScanActivity.java
+++ mobile/src/main/java/cz/uhk/fim/kikm/wearnavigation/activities/scan/ScanActivity.java
@@ -1,9 +1,12 @@
 package cz.uhk.fim.kikm.wearnavigation.activities.scan;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.qozix.tileview.TileView;
@@ -19,12 +22,20 @@ import cz.uhk.fim.kikm.wearnavigation.model.database.Fingerprint;
 import cz.uhk.fim.kikm.wearnavigation.model.database.helpers.DatabaseDataInterface;
 import cz.uhk.fim.kikm.wearnavigation.model.database.helpers.DatabaseDataLoader;
 
+/**
+ * Displays map with fingerprints and enables different actions with them.
+ * - See information about selected fingerprint
+ * - Create a new fingerprint
+ * - Delete fingerprint
+ */
 public class ScanActivity extends BaseActivity implements DatabaseDataInterface<List<Fingerprint>> {
 
     // Map view
     private TileView mMap;
     // List of the fingerprints loaded and kept for display here
     private List<Fingerprint> mFingerprints;
+    // Disables HotSpot click if marker was clicked
+    private boolean mMarkerClicked = false;
 
     // Sizes of the map
     private final int MAP_WIDTH = 3000;
@@ -34,40 +45,29 @@ public class ScanActivity extends BaseActivity implements DatabaseDataInterface<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        loadMap();
-        loadFingerprints();
+        loadMap();              // Find and load map
+        loadFingerprints();     // Loads fingerprint data
     }
 
     /**
-     * Loads map and tiles of the map. Sets all the required variables.
+     * Loads map and tiles of the map. Sets all the required variables like scale and markers.
      */
     private void loadMap() {
-        // Find the map view
-        mMap = findViewById(R.id.as_view_map);
+        mMap = findViewById(R.id.as_view_map);              // Find the map view
 
-        // Let the image explode (set scale limits)
-        mMap.setScaleLimits( 0, 2 );
-
-        // Size of original image at 100% scale
-        mMap.setSize( MAP_WIDTH, MAP_HEIGHT );
-
+        mMap.setSize( MAP_WIDTH, MAP_HEIGHT );              // Size of original image at 100% scale
         // Map detail levels for zooms
         mMap.addDetailLevel( 1.000f, "tiles/j3np/1000/j3np-%d_%d.png");
         mMap.addDetailLevel( 0.500f, "tiles/j3np/500/j3np-%d_%d.png");
         mMap.addDetailLevel( 0.250f, "tiles/j3np/250/j3np-%d_%d.png");
         mMap.addDetailLevel( 0.125f, "tiles/j3np/125/j3np-%d_%d.png");
 
-        // Set initial scale to have the map zoomed in at first
-        mMap.setScale(0.50F);
+        mMap.setScaleLimits( 0, 2 );            // Let the image explode (set scale limits)
+        mMap.setScale(0.50F);                   // Set initial scale to have the map zoomed in at first
 
-        // Lets center all markers both horizontally and vertically
-        mMap.setMarkerAnchorPoints( -0.5f, -0.5f );
-
-        // Sets listener on tap events for the markers
-        mMap.setMarkerTapListener(mMarkerTapListener);
-
-        // Define bound to be able to convert Absolute X to Relative X for clicks
-        mMap.defineBounds(0,0,MAP_WIDTH,MAP_HEIGHT);
+        mMap.setMarkerAnchorPoints( -0.5f, -0.5f );      // Lets center all markers both horizontally and vertically
+        mMap.setMarkerTapListener(mMarkerTapListener);                  // Sets listener on tap events for the markers
+        mMap.defineBounds(0,0, MAP_WIDTH, MAP_HEIGHT);         // Define bound to be able to convert Absolute X to Relative X for marker clicks
 
         // Make the whole map clickable to be able to add new pins (fingerprints)
         HotSpot hotSpot = new HotSpot();
@@ -75,20 +75,17 @@ public class ScanActivity extends BaseActivity implements DatabaseDataInterface<
         mMap.addHotSpot(hotSpot);
         mMap.setHotSpotTapListener(mHotspotTapListener);
 
-        // Frame the map to the center of the view (does not work in onCreate())
-        frameTo( 0.5, 0.5 );
+        frameTo( 0.5, 0.5 );                       // Frame the map to the center of the view (does not work in onCreate())
 
-        // Render while panning
-        mMap.setShouldRenderWhilePanning( true );
-
-        // Disallow going back to minimum scale while double-taping at maximum scale (for demo purpose)
-        mMap.setShouldLoopScale( false );
+        mMap.setShouldRenderWhilePanning( true );       // Render while panning
+        mMap.setShouldLoopScale( false );               // Disallow going back to minimum scale while double-taping at maximum scale (for demo purpose)
     }
 
     /**
      * Initiates loading of the fingerprints
      */
     private void loadFingerprints() {
+        // Connects to the database via AsyncTask and downloads basic fingerprint information
         DatabaseDataLoader<List<Fingerprint>> loader = new DatabaseDataLoader<>(this);
         loader.execute(DatabaseDataLoader.MODE_FINGERPRINT);
     }
@@ -98,19 +95,17 @@ public class ScanActivity extends BaseActivity implements DatabaseDataInterface<
      */
     private void displayFingerprints() {
         for (Fingerprint fingerprint : mFingerprints) {
-            int x = fingerprint.getX();
-            int y = fingerprint.getY();
-            double[] point = { x , y };
-
+            // Initiates image view
             ImageView iw = new ImageView(this);
-            // TODO: tag can be object so put fingerprint id in there and handle that info as you like.
-            iw.setTag(point);
-            iw.setId(fingerprint.getDbId());
+            int[] fingerprintInfo = { fingerprint.getDbId(), fingerprint.getX(), fingerprint.getY() };
+            iw.setTag(fingerprintInfo);                  // Set database id, x and y of the Fingerprint to the view.
 
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(30, 30);
-            iw.setLayoutParams(layoutParams);
-            iw.setImageResource(R.drawable.green);
+            // Sets image resource of the marker
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(84,120);
+            iw.setLayoutParams(lp);                                 // Sets layout to the ImageView
+            iw.setImageResource(R.drawable.map_marker_normal);      // Sets image resource to the view
 
+            // Adds marker to the map
             mMap.addMarker(iw, fingerprint.getX(), fingerprint.getY(), null, null);
         }
     }
@@ -121,11 +116,58 @@ public class ScanActivity extends BaseActivity implements DatabaseDataInterface<
      */
     private MarkerLayout.MarkerTapListener mMarkerTapListener = new MarkerLayout.MarkerTapListener() {
         @Override
-        public void onMarkerTap(View v, int x, int y ) {
-            double[] position = (double[]) v.getTag();
-            Toast.makeText( ScanActivity.this, "You tapped a pin (id = " + v.getId() + ") at " + position[0] + ":" + position[1], Toast.LENGTH_SHORT ).show();
+        public void onMarkerTap(View markerView, int x, int y ) {
+            mMarkerClicked = true;           // Set to disable HotSpot click to trigger
+
+            // Adds callout layout to the map
+            int[] data = (int[]) markerView.getTag();         // Position of the callout widget
+            View markerActions = generateMarkerActionView(data[0]);  // Generates layout of the callout widget
+            mMap.addCallout( markerActions, data[1], data[2], -0.5f, -1.0f );
         }
     };
+
+    /**
+     * Generates view that enables user to show more information about Fingerprint.
+     * Also enables to delete Fingerprint from the database.
+     *
+     * @param fingerprintDbId database id of fingerprint
+     * @return View with the actions
+     */
+    private View generateMarkerActionView(final int fingerprintDbId) {
+        // Load view via inflater
+        LayoutInflater inflater = LayoutInflater.from(ScanActivity.this);
+        View calloutView = inflater.inflate(R.layout.actions_marker, null);
+
+        // Button to show more information about selected fingerprint.
+        ImageButton info = calloutView.findViewById(R.id.am_show_info);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( ScanActivity.this, "Show information fragment with selected fingerprint (id = " + fingerprintDbId + " ) information.", Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        // Delete button function to delete selected Fingerprint.
+        ImageButton delete = calloutView.findViewById(R.id.am_delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( ScanActivity.this, "Delete fingerprint (id = " + fingerprintDbId + " ).", Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        // Cancel button in markerView
+        ImageButton cancel = calloutView.findViewById(R.id.am_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Remove single does not work :(
+                mMap.getCalloutLayout().removeAllViews();
+            }
+        });
+
+        return calloutView;
+    }
 
     /**
      * OnClickListener for the HotSpots (whole map)
@@ -134,12 +176,62 @@ public class ScanActivity extends BaseActivity implements DatabaseDataInterface<
     private HotSpot.HotSpotTapListener mHotspotTapListener = new HotSpot.HotSpotTapListener() {
         @Override
         public void onHotSpotTap(HotSpot hotSpot, int x, int y) {
-            CoordinateTranslater ct = mMap.getCoordinateTranslater();
-            int realX = (int) ct.translateAndScaleAbsoluteToRelativeX(x, mMap.getScale());
-            int realY = (int) ct.translateAndScaleAbsoluteToRelativeY(y, mMap.getScale());
-            Toast.makeText( ScanActivity.this, "You tapped at position: " + realX + ":" + realY, Toast.LENGTH_SHORT ).show();
+            // Dos not show anything if Marker was clicked
+            if(!mMarkerClicked) {
+                CoordinateTranslater ct = mMap.getCoordinateTranslater();
+                int realX = (int) ct.translateAndScaleAbsoluteToRelativeX(x, mMap.getScale());
+                int realY = (int) ct.translateAndScaleAbsoluteToRelativeY(y, mMap.getScale());
+                //Toast.makeText(ScanActivity.this, "You tapped at position: " + realX + ":" + realY, Toast.LENGTH_SHORT).show();
+
+                // Round both numbers to tens to make the steps little easier
+                int roundedX = (int) Math.round(realX/10.0) * 10;
+                int roundedY = (int) Math.round(realY/10.0) * 10;
+
+                // Adds callout layout to the map
+                View markerActions = generateNewMarkerActionView(roundedX, roundedY);  // Generates layout of the callout widget
+                mMap.addCallout( markerActions, roundedX, roundedY, -0.5f, -1.0f );
+            }
+            mMarkerClicked = false;             // Reset marker click variable
         }
     };
+
+    /**
+     * Generates view that enables user to create new Fingerprint.
+     *
+     * @param posX of the new fingerprint
+     * @param posY of the new fingerprint
+     * @return View of the action menu
+     */
+    private View generateNewMarkerActionView(int posX, int posY) {
+        // Load view via inflater
+        LayoutInflater inflater = LayoutInflater.from(ScanActivity.this);
+        View calloutView = inflater.inflate(R.layout.actions_marker_new, null);
+
+        // Sets position text to the view
+        TextView textPosition = calloutView.findViewById(R.id.amn_position);
+        textPosition.setText(String.format(getResources().getString(R.string.amn_position), posX, posY));
+
+        // Button to create new fingerprint.
+        ImageButton info = calloutView.findViewById(R.id.amn_create);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( ScanActivity.this, "Add new fingerprint", Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        // Cancel button in markerView
+        ImageButton cancel = calloutView.findViewById(R.id.amn_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Remove single does not work :(
+                mMap.getCalloutLayout().removeAllViews();
+            }
+        });
+
+        return calloutView;
+    }
 
     /**
      * This is a convenience method to scrollToAndCenter after layout (which won't happen if called directly in onCreate
---
+++ mobile/src/main/java/cz/uhk/fim/kikm/wearnavigation/utils/MarkerOptions.java
@@ -0,0 +1,18 @@
+package cz.uhk.fim.kikm.wearnavigation.utils;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import cz.uhk.fim.kikm.wearnavigation.R;
+
+public class MarkerOptions extends ConstraintLayout {
+    public MarkerOptions(Context context) {
+        super(context);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //View view = inflater.inflate(R.layout.actions_marker, );
+        //addView(view);
+    }
+}
